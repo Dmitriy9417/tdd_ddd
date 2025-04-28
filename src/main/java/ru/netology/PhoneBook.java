b44dbe3d95d465bcae697de0dd2390a1d774fe6a
Dimi <rub.dimas@yandex.ru>
2025-04-28 19:02:39 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,23 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
 public class PhoneBook {
+    private final Map<String, String> nameToNumber = new HashMap<>();
+    private final Map<String, String> numberToName = new HashMap<>();
+    private final TreeSet<String> names = new TreeSet<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (nameToNumber.containsKey(name)) {
+            return nameToNumber.size();
+        }
+
+        nameToNumber.put(name, number);
+        numberToName.put(number, name);
+        names.add(name);
+
+        return nameToNumber.size();
     }
 }
