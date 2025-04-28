f94cf070029b1f03c6059d650a47798b7b2fea6a
Dimi <rub.dimas@yandex.ru>
2025-04-28 19:09:11 +0300

    Stub and tests for findByNumber

--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class PhoneBookTest {
     @Test
@@ -17,5 +18,15 @@ public class PhoneBookTest {
         PhoneBook phoneBook = new PhoneBook();
         assertEquals(1, phoneBook.add("Alice", "12345"));
         assertEquals(1, phoneBook.add("Alice", "54321")); // не должен добавить
+    }
+    @Test
+    public void testFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Alice", "12345");
+        phoneBook.add("Bob", "67890");
+
+        assertEquals("Alice", phoneBook.findByNumber("12345"));
+        assertEquals("Bob", phoneBook.findByNumber("67890"));
+        assertNull(phoneBook.findByNumber("99999")); // несуществующий номер
     }
 }
