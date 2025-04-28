3d55625e4eb99fa7130ec30ed54a422f709804be
Dimi <rub.dimas@yandex.ru>
2025-04-28 19:01:26 +0300

    Stub and tests for add

--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,4 +1,21 @@
 package ru.netology;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAddNewContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Alice", "12345"));
+        assertEquals(2, phoneBook.add("Bob", "67890"));
+    }
+
+    @Test
+    public void testAddDuplicateName() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Alice", "12345"));
+        assertEquals(1, phoneBook.add("Alice", "54321")); // не должен добавить
+    }
 }
